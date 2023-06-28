09af557986b57564b90931672028c0896d618c02
Matej Pavic <mpavic.dev@gmail.com>
2023-06-29 00:52:52 +0200

    refactor app to use services module

--- src/controllers/jokes.ts
+++ src/controllers/jokes.ts
@@ -1,10 +1,10 @@
 import {Request, Response} from 'express';
 import { ERROR_MESSAGES, STATUS } from '../constants';
+import { getJoke } from '../services/jokes';
 
-export const getJoke = async (_req: Request, res: Response) => {
+export const getJokeController = async (_req: Request, res: Response) => {
   try {
-    const response = await fetch('https://api.chucknorris.io/jokes/random');
-    const joke = await response.json();
+    const joke = await getJoke();
     res.status(STATUS.OK).json(joke.value);
   } catch (error) {
     res.status(STATUS.INTERNAL_SERVER_ERROR).json({ error: ERROR_MESSAGES.FAILED_GET_USERS });
--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,28 +1,21 @@
 import {Request, Response} from 'express';
-import { User } from '../entity/User';
-import { AppDataSource } from '../data-source';
 import { ERROR_MESSAGES, STATUS } from '../constants';
-import bcrypt from 'bcrypt';
+import { createUser, getUsers } from '../services/users';
 
-export const getUsers = async (req: Request, res: Response) => {
+export const getUsersController = async (_req: Request, res: Response) => {
   try {
-    const userRepository = AppDataSource.getRepository(User);
-    const users = await userRepository.find();
+    const users = await getUsers();
     res.status(STATUS.OK).json(users);
   } catch (error) {
     res.status(STATUS.INTERNAL_SERVER_ERROR).json({ error: ERROR_MESSAGES.FAILED_GET_USERS });
   }
 }
 
-export const createUser = async (req: Request, res: Response) => {
+export const createUserController = async (req: Request, res: Response) => {
   try {
-    const userRepository = AppDataSource.getRepository(User);
-    const { email, password, firstName, lastName } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = userRepository.create({ email, password: hashedPassword, firstName, lastName });
-    await userRepository.save(newUser);
+    const newUser = await createUser(req.body);
     res.status(STATUS.CREATED).json(newUser);
   } catch (error) {
     res.status(STATUS.INTERNAL_SERVER_ERROR).json({ error: ERROR_MESSAGES.FAILED_CREATE_USER });
   }
-}
+};
