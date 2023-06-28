a03450d8345fb90531e389ffac3eb6a21febb8d0
Matej Pavic <mpavic.dev@gmail.com>
2023-06-29 00:23:51 +0200

    change file naming

--- src/routes/auth.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import { createUser, getUsers } from '../controllers/user';
-
-router.get('/users', getUsers);
-router.post('/users', createUser);
-
-export default router;
