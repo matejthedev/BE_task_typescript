5c99617910a03293ffacca3c068a31762097fe31
Matej Pavic <mpavic.dev@gmail.com>
2023-06-29 11:54:37 +0200

    moving the business logic to the service

--- src/controllers/login.ts
+++ src/controllers/login.ts
@@ -1,29 +1,13 @@
 import { Request, Response } from 'express';
-import bcrypt from 'bcrypt';
-import { AppDataSource } from '../data-source';
-import { User } from '../entity/User';
-import { createJWT } from '../utils/jwt';
-import { SUCCESS_MESSAGES, ERROR_MESSAGES } from '../constants';
+import { STATUS } from '../constants';
+import { login } from '../services/login';
 
 export const loginController = async (req: Request, res: Response) => {
   try {
     const { email, password } = req.body;
-
-    const userRepository = AppDataSource.getRepository(User);
-    const user = await userRepository.findOne({ where: { email } });
-    if (!user) throw new Error(ERROR_MESSAGES.FAILED_GET_USER);
-
-    const isPasswordCorrect = bcrypt.compareSync(password, user.password);
-  
-    if (!isPasswordCorrect) throw new Error(ERROR_MESSAGES.INVALID_PASSWORD);
-  
-    const token = createJWT(user);
-  
-    return res.status(200).json({
-      message: SUCCESS_MESSAGES.USER_LOGGED_IN,
-      token
-    });
-  } catch (error) {
-    console.log(error)
+    const responseObject = await login(email, password);
+    res.status(STATUS.OK).json(responseObject);
+  } catch (error: any) {
+    res.status(STATUS.BAD_REQUEST).json(error.message);
   }
 };
---
+++ src/services/login.ts
@@ -0,0 +1,19 @@
+import bcrypt from 'bcrypt';
+import { createJWT } from '../utils/jwt';
+import { SUCCESS_MESSAGES, ERROR_MESSAGES } from '../constants';
+import { getUserByEmail } from './users';
+
+export const login = async (email: string, password: string) => {
+    const user = await getUserByEmail(email);
+    if (!user) throw new Error(ERROR_MESSAGES.INVALID_EMAIL)
+    const isPasswordCorrect = bcrypt.compareSync(password, user.password);
+  
+    if (!isPasswordCorrect) throw new Error(ERROR_MESSAGES.INVALID_PASSWORD);
+  
+    const token = createJWT(user);
+
+    return {
+      message: SUCCESS_MESSAGES.USER_LOGGED_IN,
+      token
+    };
+};
