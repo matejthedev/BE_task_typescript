4a4ddf660978b20bb01c127f57bd059cc5d6f474
Matej Pavic <mpavic.dev@gmail.com>
2023-06-29 11:53:58 +0200

    refactor util jwt function to use fewer lines

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -8,13 +8,8 @@ type User = {
   password: string;
 };
 
-const SECRET_KEY = process.env.JWT_SECRET_KEY;
-const EXPIRATION_TIME = process.env.JWT_EXPIRATION;
-
 export const createJWT = (user: User) => jwt.sign(
   { id: user.id, email: user.email },
-  SECRET_KEY as string,
-  { expiresIn: EXPIRATION_TIME }
+  process.env.JWT_SECRET_KEY as string,
+  { expiresIn: process.env.JWT_EXPIRATION }
 );
-
-exports.createJWT = createJWT;
